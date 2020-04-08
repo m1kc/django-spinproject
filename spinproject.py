ba5bac681179312ba1150ef5cb8aec285db1409a
m1kc (Max Musatov) <m1kc@yandex.ru>
2020-04-08 10:06:58 +0300

    Make the whole thing a bit more modular

--- spinproject.py
+++ spinproject.py
@@ -13,15 +13,22 @@ name = argv[0]
 path = argv[0]
 if len(argv) == 2:
 	path = argv[1]
-	
-print(f"Creating project `{name}`, path: {path}")
+
+print(f"Creating project `{name}`, path: `{path}`")
 
 subprocess.run(['mkdir', '-p', path], check=True)
 subprocess.run(['django-admin', 'startproject', name, path], check=True)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-subprocess.run([os.path.join(BASE_DIR, 'enhance-srta.py'), name, path], check=True)
-subprocess.run([os.path.join(BASE_DIR, 'enhance-dockerfile.py'), name, path], check=True)
-subprocess.run([os.path.join(BASE_DIR, 'enhance-makefile.py'), name, path], check=True)
-subprocess.run([os.path.join(BASE_DIR, 'enhance-settings.py'), name, path], check=True)
+
+active_modules = [
+	'srta',
+	'dockerfile',
+	'makefile',
+	'settings',
+	# 'pytest',
+]
+
+for m in active_modules:
+	subprocess.run([os.path.join(BASE_DIR, f'enhance-{m}.py'), name, path], check=True)
