979fafcc3385c9406b28adb9de0ea4b537714bd3
m1kc (Max Musatov) <m1kc@yandex.ru>
2021-04-16 16:53:10 +0300

    Add `make clean` support

--- django_spinproject/bin/enhance-makefile.py
+++ django_spinproject/bin/enhance-makefile.py
@@ -9,8 +9,11 @@ assert len(argv) == 2, 'Must provide: name, path'
 name, path = argv
 
 
-template = f'''lint:
+template = '''lint:
 	flake8 --select=C,F,E101,E112,E502,E72,E73,E74,E9,W291,W6 --exclude=.cache,migrations
+
+clean:
+	find -depth -name '__pycache__' -exec rm -rfv '{}' ';'
 
 
 docker:
