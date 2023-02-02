bb270c20b7096029c00b321348147440bf5edfb3
Kirill_Lekhov <k.lekhov@comearth.ru>
2023-02-02 16:12:10 +0300

    Upgrade dockerignore and gitlab-ci

---
+++ django_spinproject/modules/dockerignore_data/v3.py
@@ -0,0 +1,33 @@
+_CONTENT = {
+	'.dockerignore': '''*.sqlite3
+*.sqlite3-journal
+
+.env
+main/.env
+**/.env
+
+**/__pycache__
+**/.cache
+**/.classpath
+**/.dockerignore
+**/.env
+**/.git
+**/.gitignore
+**/.project
+**/.settings
+**/.toolstarget
+**/.vs
+**/.vscode
+**/*.*proj.user
+**/*.dbmdl
+**/*.jfm
+**/azds.yaml
+**/charts
+**/docker-compose*
+**/Dockerfile*
+**/node_modules
+**/npm-debug.log
+**/obj
+**/secrets.dev.yaml
+**/values.dev.yaml'''
+}
