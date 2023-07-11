c16f6d600860eaaa60c14ae86380cba2786802fc
m1kc (Max Musatov) <m1kc@yandex.ru>
2023-07-11 19:22:59 +0400

    login-template: slightly improve form looks

--- django_spinproject/modules/login_template_data/v1.py
+++ django_spinproject/modules/login_template_data/v1.py
@@ -3,15 +3,19 @@ _CONTENT = {
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Title</title>
+	<title>Authentication</title>
+	<style>
+		body { margin: 2em auto; max-width: 20em }
+		input[type=submit] { width: 100%; margin-top: 1em }
+	</style>
 </head>
 <body>
 	{% block content %}
-	
+
 		{% if form.errors %}
-			<p>Your username and password didn't match. Please try again.</p>
+			<p style="color:darkred">Your username and password didn't match. Please try again.</p>
 		{% endif %}
-		
+
 		{% if next %}
 			{% if user.is_authenticated %}
 				<p>Your account doesn't have access to this page. To proceed, please login with an account that has access.</p>
@@ -19,29 +23,29 @@ _CONTENT = {
 				<p>Please login to see this page.</p>
 			{% endif %}
 		{% endif %}
-		
+
 		<form method="post" action="{% url 'login' %}">
 			{% csrf_token %}
 		<table>
-		
+
 		<tr>
 			<td>{{ form.username.label_tag }}</td>
 			<td>{{ form.username }}</td>
 		</tr>
-		
+
 		<tr>
 			<td>{{ form.password.label_tag }}</td>
 			<td>{{ form.password }}</td>
 		</tr>
 		</table>
-		
-		<input type="submit" value="login" />
+
+		<input type="submit" value="Log in" />
 		<input type="hidden" name="next" value="{{ next }}" />
 		</form>
-		
+
 		{# Assumes you setup the password_reset view in your URLconf #}
-		<p><a href="{% url 'password_reset' %}">Lost password?</a></p>
-	
+		{# <p><a href="{% url 'password_reset' %}">Lost password?</a></p> #}
+
 	{% endblock content %}
 </body>
 </html>
